b6eda31269c6bce57b11f914b9b31f5778dc4dce
Shota Shimazu <hornet.live.mf@gmail.com>
2020-03-12 20:24:07 +0900

    remove unused flag

--- Sources/Versions/main.swift
+++ Sources/Versions/main.swift
@@ -5,12 +5,6 @@ struct VersionsCLI: ParsableCommand {
     @Argument(help: "Specify language")
     var language: String
 
-    @Flag(help: "Include a counter with each repetition.")
-    var includeCounter: Bool
-
-    @Option(name: .shortAndLong, help: "The number of times to repeat 'phrase'.")
-    var count: Int?
-
     func readVersinfiles() {
         let file = FileHandle(forReadingAtPath: "./Versionfile")!
         let data = file.readDataToEndOfFile()
