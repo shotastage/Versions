82c4ed14797ebbb65a8376e49a69cbf9d4085d55
Shota Shimazu <hornet.live.mf@gmail.com>
2020-03-15 23:07:15 +0900

    revise

--- Sources/Versions/VersionFiler.swift
+++ Sources/Versions/VersionFiler.swift
@@ -4,20 +4,23 @@ class VersionFiler {
     let fileManager = FileManager.default
 
     func xcodeVersion() {
-        let result = fileManager.createFile(atPath: ".xcode-version", contents: nil, attributes: nil)
         let ver = Version(of: "xcodebuild -version").sliceLine(of: 1).register(rule: "[throw] [number]").number
 
         write(file: URL(fileURLWithPath: ".xcode-version"), text: ver)
     }
 
+    /*
     func swiftVersion() {
+        let ver = Version(of: "xcodebuild -version").sliceLine(of: 1).register(rule: "[throw] [number]").number
+
         write(file: URL(fileURLWithPath: ".swift-version"), text: ver)
     }
 
     func rubyVersion() {
         write(file: URL(fileURLWithPath: ".ruby-version"), text: ver)
     }
-
+    */
+    
     @discardableResult
     private func write(file: URL, text: String) -> Bool {
         guard let stream = OutputStream(url: file, append: true) else {
