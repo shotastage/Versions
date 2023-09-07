e61a24e2064f0967de40256dd5ad6e9236e5f89d
shotastage <shota.shimazu@magicalsoft.app>
2023-09-07 11:39:15 +0900

    upgrade swift version

--- Package.swift
+++ Package.swift
@@ -1,28 +1,23 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Versions",
-    products: [
-        .executable(name: "versions", targets: ["Versions"])
-    ],
+    name: "versions",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "Versions",
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "versions",
             dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]),
-        .testTarget(
-            name: "VersionsTests",
-            dependencies: ["Versions"]),
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ],
+            path: "Sources"
+        )
     ]
 )
